fdd53edfcacf34a350e85fb89d51e3a8e499ade0
xellox <korpasdarius911@gmail.com>
2023-06-16 15:52:23 +0300

    modified:   index.html 	modified:   java.js 	modified:   products.js 	modified:   style.css

--- products.js
+++ products.js
@@ -1,86 +1,63 @@
 const products = [
-    {
-        id:0,
-        name: "Coffee 1",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p1.png",
-        numberOfUnits: 2,
-    },
+  {
+    id: 0,
+    name: "Coffee 1",
+    price: 29.95,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet.",
+    imgSrc: "./images/p1.png",
+    numberOfUnits: 2,
+  },
 
-    {
-        id:1,
-        name: "Coffee 2",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p2.png",
-    },
+  {
+    id: 1,
+    name: "Coffee 2",
+    price: 27.25,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit, amet.",
+    imgSrc: "./images/p2.png",
+  },
 
-    {
-        id:2,
-        name: "Coffee 3",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p3.png",
-    },
+  {
+    id: 2,
+    name: "Coffee 3",
+    price: 33.95,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit, amet.",
+    imgSrc: "./images/p3.png",
+  },
 
-    {
-        id:3,
-        name: "Coffee 4",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p4.png",
-    },
+  {
+    id: 3,
+    name: "Coffee 4",
+    price: 25.95,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit, amet.",
+    imgSrc: "./images/p4.png",
+  },
 
-    {
-        id:4,
-        name: "Coffee 5",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p5.png",
-    },
+  {
+    id: 4,
+    name: "Coffee 5",
+    price: 37,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit, amet.",
+    imgSrc: "./images/p5.png",
+  },
 
-    {
-        id:5,
-        name: "Coffee 6",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p6.png",
-    },
+  {
+    id: 5,
+    name: "Coffee 6",
+    price: 40.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit, amet.",
+    imgSrc: "./images/p6.png",
+  },
 
-    {
-        id:5,
-        name: "Coffee 6",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p6.png",
-    },
-
-    {
-        id:5,
-        name: "Coffee 6",
-        price: 29.99,
-        instock: 100,
-        description: 
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, architecto?",
-        imgSrc: "./images/p6.png",
-    },
-
-    
-
-    
-]
+];
